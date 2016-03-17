e47a70d5360af861ea9c0fff999893605406ce99
Bruce Hoang <bhoang2@ualberta.ca>
2016-03-17 14:15:21 -0600

    hello

---
+++ app/TweetListTest.java
@@ -0,0 +1,48 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by joshua2 on 9/28/15.
+ */
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+    public TweetListTest() {
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet() {
+        TweetList list = new TweetList();
+        list.add(new NormalTweet("test"));
+    }
+
+    public void testDelete() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        list.delete(tweet);
+        assertFalse(list.contains(tweet));
+    }
+
+    public void testContains() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        assertTrue(list.contains(tweet));
+    }
+
+    public void testSingleton() {
+        //TODO write code for tests!
+        TweetList list = new TweetList();
+        list.add(new NormalTweet("test"));
+
+        assertEquals(list.size(), 1);
+    }
+    public void testSingleton2() {
+        TweetList list = TweetList.getInstance();
+        list.add(new NormalTweet("test"));
+
+        assertEquals(list.size(), 1);
+    }
+}
